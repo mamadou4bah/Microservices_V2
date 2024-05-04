b7e0706a95d8d9b864391572064b88745f801590
mamadou4bah <bahmamadouwansan@gmail.com>
2024-05-04 23:39:48 +0200

    Section4: Handle deployment, portability & scalability of microservices using Docker

---
+++ section4/accounts/src/main/java/com/mamadou4bah/cards/mapper/CustomerMapper.java
@@ -0,0 +1,22 @@
+package com.mamadou4bah.cards.mapper;
+
+import com.mamadou4bah.cards.dto.CustomerDto;
+import com.mamadou4bah.cards.entity.Customer;
+
+public class CustomerMapper {
+
+    public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
+        customerDto.setName(customer.getName());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setMobileNumber(customer.getMobileNumber());
+        return customerDto;
+    }
+
+    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        return customer;
+    }
+
+}
---
+++ section4/cards/src/main/java/com/mamadou4bah/cards/CardsApplication.java
@@ -0,0 +1,42 @@
+package com.mamadou4bah.cards;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.mamadou4bah.cards.controller") })
+@EnableJpaRepositories("com.mamadou4bah.cards.repository")
+@EntityScan("com.mamadou4bah.cards.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Bmwas microservice REST API Documentation",
+				description = "Bmwas Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Mamadou BAH",
+						email = "tutor@bmwas.com",
+						url = "https://www.bmwas.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.bmwas.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Bmwas Cards microservice REST API Documentation",
+				url = "https://www.Bmwas.com/swagger-ui.html"
+		)
+)
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+}
