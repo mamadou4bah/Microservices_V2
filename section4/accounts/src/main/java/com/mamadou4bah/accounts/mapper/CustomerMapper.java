52bdbfe8ddbfea91d11d0df83d7dc958f2553e87
mamadou4bah <bahmamadouwansan@gmail.com>
2024-05-19 11:10:42 +0200

    Section6 => Configuration Management in microservices: spring cloud, webhouk github, rabbitmq, etc ...

---
+++ section4/accounts/src/main/java/com/mamadou4bah/accounts/mapper/CustomerMapper.java
@@ -0,0 +1,22 @@
+package com.mamadou4bah.accounts.mapper;
+
+import com.mamadou4bah.accounts.dto.CustomerDto;
+import com.mamadou4bah.accounts.entity.Customer;
+
+public class CustomerMapper {
+
+    public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
+        customerDto.setName(customer.getName());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setMobileNumber(customer.getMobileNumber());
+        return customerDto;
+    }
+
+    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        return customer;
+    }
+
+}
---
+++ section6/v1-springboot/accounts/src/main/java/com/mamadou4bah/accounts/AccountsApplication.java
@@ -0,0 +1,44 @@
+package com.mamadou4bah.accounts;
+
+import com.mamadou4bah.accounts.dto.AccountsContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "Bmwas Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Mamadou BAH",
+						email = "tutor@bmwas.com",
+						url = "https://www.bmwas.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.bmwas.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "Bmwas Accounts microservice REST API Documentation",
+				url = "http://localhost:8080/swagger-ui/index.html"
+		)
+)
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
