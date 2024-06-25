feb6482cc7ad5d348a9aba0ffa5d16118a7fd35e
mamadou4bah <bahmamadouwansan@gmail.com>
2024-06-25 23:32:30 +0200

    Section_11 => Observability with Loki, Promtail, Grafana and Metrics/Monitoring with Actuator, Micrometer, Prometheus, Grafana, ...

---
+++ section_11/accounts/src/main/java/com/mamadou4bah/accounts/service/client/CardsFallback.java
@@ -0,0 +1,14 @@
+package com.mamadou4bah.accounts.service.client;
+
+import com.mamadou4bah.accounts.dto.CardsDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CardsFallback implements CardsFeignClient {
+
+    @Override
+    public ResponseEntity<CardsDto> fetchCardDetails(String correlationId, String mobileNumber) {
+        return null;
+    }
+}
